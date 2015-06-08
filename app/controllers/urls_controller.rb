fa99835846abb859b7a1c521a0a0993d067badda
Rodrigo Gouvêa <rodrigosggouvea@gmail.com>
2015-06-08 16:32:26 +0000

    long url

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,5 +1,6 @@
 class UrlsController < ApplicationController
   def create
-    @url = params.require(:url).permit(:long)
+    url = params.require(:url).permit(:long)
+    @url = Shortener::ShortenedUrl.generate(url[:long])
   end
 end
